e4648c9579edc32c3c5868c9c2e6e4b3afcfd683
Chuan <chuan.l.hung@gmail.com>
2021-11-17 14:41:06 -1000

    added Google Analytics tag

--- app_base.py
+++ app_base.py
@@ -58,6 +58,15 @@ app.index_string = '''
         {%favicon%}
         {%css%}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7896505577932847" crossorigin="anonymous"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S4GCJ81M53"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-S4GCJ81M53');
+        </script>
     </head>
     <body>
         {%app_entry%}
